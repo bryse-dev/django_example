9b477f149793839a5cfa521cfaed5ac04c5d0f39
Bryson Earl <bryson.earl@sprint.com>
2018-02-20 16:27:12 -0500

    Added additional fields to response

--- simple_search/api/models.py
+++ simple_search/api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 MATCHER_ENUM = [("MATCHER1", "search matcher 1"), ("MATCHER2", "search matcher 2")]
+STATUS_ENUM = [("CREATED", "step one of searching"), ("RUNNING", "step two of searching")]
 
 
 class Search(models.Model):
     name = models.CharField(max_length=100)
     matcher = models.CharField(choices=MATCHER_ENUM, max_length=64)
+    status = models.CharField(choices=STATUS_ENUM, default="CREATED", max_length=64)
+    percent_complete = models.FloatField(default=0)
--- simple_search/api/serializers.py
+++ simple_search/api/serializers.py
@@ -5,20 +5,5 @@ from simple_search.api.models import Search, MATCHER_ENUM
 class SearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Search
-        fields = ('name', 'matcher')
-    # name = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    # matcher = serializers.ChoiceField(choices=MATCHER_ENUM, required=True)
-
-    # def create(self, validated_data):
-    #     """
-    #     Create and return a Search object using parsed data
-    #     """
-    #     return Search.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     """
-    #     Update and return a Search objects
-    #     """
-    #     instance.name = validated_data.get('name', instance.name)
-    #     instance.matcher = validated_data.get('matcher', instance.matcher)
-    #     return instance
+        fields = ('name', 'matcher', 'status', 'percent_complete')
+        read_only_fields = ('status', 'percent_complete',)
